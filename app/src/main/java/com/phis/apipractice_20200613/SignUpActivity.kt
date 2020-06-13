26726837502993082a56ed7ac732301a1ad8fc0d
parkcrystal <9jeworld@naver.com>
2020-06-13 14:13:37 +0900

    회원가입

--- app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.phis.apipractice_20200613.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -23,11 +24,26 @@ class SignUpActivity : BaseActivity() {
             val inputEmail = emailEdt.text.toString()
 
             ServerUtil.getRequestDuplicatedCheck(mContext, "EMAIL", "inputEmail", object :ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
 
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        runOnUiThread {
+                            emailCheckBtnResultTxt.text = "사용해도 좋습니다."
+                        }
+
+    
+                    }
+                    else {
+                        runOnUiThread {
+                            emailCheckBtnResultTxt.text = "이미 사용중입니다. 다른 이메일로 다시 체크해주세요."
+                        }
+                    }
+
+                }
 
             })
-
-
 
 
         }
