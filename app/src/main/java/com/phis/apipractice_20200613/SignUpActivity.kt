ad8db14a08d13e9fc0eb146d840e345c8884dec4
parkcrystal <9jeworld@naver.com>
2020-06-13 14:41:48 +0900

    닉네임 중복확인 기능 완료함.

--- app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.phis.apipractice_20200613
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.phis.apipractice_20200613.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -19,11 +20,43 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+
+        nickNameCheckBtn.setOnClickListener {
+
+            val inputNickName = nickNameEdt.text.toString()
+
+            ServerUtil.getRequestDuplicatedCheck(mContext, "NICKNAME", inputNickName, object :ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+
+                    Log.d("화면에서 보는 응답", json.toString())
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        runOnUiThread {
+                            nickNameCheckBtnResultTxt.text = "사용해도 좋은 닉네임입니다."
+                        }
+
+
+                    }
+                    else {
+                        runOnUiThread {
+                            nickNameCheckBtnResultTxt.text = "중복된 닉네임입니다. 다른 닉네임을 사용해주세요."
+                        }
+                    }
+
+                }
+
+            })
+
+        }
+
         emailCheckBtn.setOnClickListener {
 
             val inputEmail = emailEdt.text.toString()
 
-            ServerUtil.getRequestDuplicatedCheck(mContext, "EMAIL", "inputEmail", object :ServerUtil.JsonResponseHandler {
+            ServerUtil.getRequestDuplicatedCheck(mContext, "EMAIL", inputEmail, object :ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 
                     val code = json.getInt("code")
