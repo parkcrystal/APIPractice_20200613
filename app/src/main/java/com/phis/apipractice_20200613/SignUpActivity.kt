f20e2bc1a971084fe865ea78aae5f1edf2cb7d46
parkcrystal <9jeworld@naver.com>
2020-06-13 15:13:54 +0900

    회원가입 버튼 누르면 중복체크 통과 여부 검사  => 모두 통과했으면 서버에 보내줄 데이터 추출

--- app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200613/SignUpActivity.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.widget.Toast
 import com.phis.apipractice_20200613.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.emailEdt
+import kotlinx.android.synthetic.main.activity_sign_up.signUpBtn
 import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
+
+//    이메일과 닉네임의 중복검사 결과를 저장하는 변수
+    var isEmailDuplOk = false
+    var isNickNameDuplOk = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +32,35 @@ class SignUpActivity : BaseActivity() {
 
 //      회원가입 버튼 누르기
         signUpBtn.setOnClickListener {
+
+//            이메일 중복검사 통과 + 닉네임 중복검사 통과했는지 확인!
+            if(!isEmailDuplOk) {
+//                이메일 중복검사 통과 못함.
+                Toast.makeText(mContext, "이메일 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
+
+
+//                뒤의 로직 실행하지 않고 이 함수를 강제 종료.
+                return@setOnClickListener
+            }
+
+            if(!isNickNameDuplOk) {
+//                닉네임 중복검사 통과 못함.
+                Toast.makeText((mContext, "닉네임 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
+
+
+//                뒤의 로직 실행하지 않고 이 함수를 강제 종료.
+                return@setOnClickListener
+            }
+
+//            여기가 실행이 되면 => 강제종료 두번을 모두 피했다. => (이메일과 닉네임 중복검사를 모두 통과함.)
+
+//            입력한 이메일 / 비번 / 닉네임을 들고 서버에 가입 신청.
+            val email = emailEdt.text.toString()
+            val pw = pwEdt.text.toString()
+            val nickname = nickNameEdt.text.toString()
+
+
+//            서버에 /user - PUT으로 요청 => ServerUtil을 통해 요청하자.
 
             
         }
@@ -41,6 +78,7 @@ class SignUpActivity : BaseActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 nickNameCheckBtnResultTxt.text = "닉네임 중복검사를 해주세요."
+                isNickNameDuplOk = false
             }
 
 
@@ -62,6 +100,7 @@ class SignUpActivity : BaseActivity() {
 
 //                이메일 중복검사 하라고 안내.
                 emailCheckBtnResultTxt.text = "이메일 중복검사를 해주세요."
+                isEmailDuplOk = false
             }
 
 
@@ -83,6 +122,7 @@ class SignUpActivity : BaseActivity() {
                     if (code == 200) {
                         runOnUiThread {
                             nickNameCheckBtnResultTxt.text = "사용해도 좋은 닉네임입니다."
+                            isNickNameDuplOk = true
                         }
 
 
@@ -111,6 +151,8 @@ class SignUpActivity : BaseActivity() {
                     if (code == 200) {
                         runOnUiThread {
                             emailCheckBtnResultTxt.text = "사용해도 좋습니다."
+//                       중복검사 결과를 true로 변경
+                            isEmailDuplOk = true
                         }
 
     
