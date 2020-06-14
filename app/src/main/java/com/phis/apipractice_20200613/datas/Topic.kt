7d43ee70965f8bcd79cab426fd47e37d720d00d4
parkcrystal <9jeworld@naver.com>
2020-06-14 14:55:53 +0900

    주제의 선택진영도 같이 파싱되도록  => 주제를 파싱하는 중에 sides JSONArray 파싱  => 주제의 진영 목록으로 추가

--- app/src/main/java/com/phis/apipractice_20200613/datas/Topic.kt
+++ app/src/main/java/com/phis/apipractice_20200613/datas/Topic.kt
@@ -16,6 +16,20 @@ class Topic {
             topic.title = json.getString("title")
             topic.imageUrl = json.getString("img_url")
 
+//            주제 파싱중 => 선택진영들 JSONArray 가져오기
+            val sides = json.getJSONArray("sides")
+//            JSONArray 내부를 돌면서 파싱
+            for(i in 0 until sides.length()){
+//                선택진영 하나의 정보가 담긴 JSONObject 추출
+                val sideJson = sides.getJSONObject(i)
+
+//                이 JSON을 선택진영으로 변환해주는 기능 사용
+                val side = TopicSide.getTopicSideFromJson(sideJson)
+
+//                이 주제의 진영 목록으로 추가
+                topic.sides.add(side)
+            }
+
 
             return topic
 
@@ -27,6 +41,9 @@ class Topic {
     var id = 0
     var title = ""
     var imageUrl = ""
+    
+//    선택 가능 진영 목록을 담는 배열
+    var sides = ArrayList<TopicSide>()
 
 
 }
