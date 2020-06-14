3d3eec03c39dc2c096cab3fd27f2f9a42b1bfb3a
parkcrystal <9jeworld@naver.com>
2020-06-14 12:47:47 +0900

    어댑터에서 실제 제목 / 이미지 (Glide) 뿌려주기

--- app/src/main/java/com/phis/apipractice_20200613/adapters/TopicAdapter.kt
+++ app/src/main/java/com/phis/apipractice_20200613/adapters/TopicAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.phis.apipractice_20200613.R
 import com.phis.apipractice_20200613.datas.Topic
 
@@ -22,6 +24,20 @@ class TopicAdapter(val mContext: Context,
             tempRow = inf.inflate(R.layout.topic_list_item, null)
         }
         val row = tempRow!!
+
+
+//        리스트 아이템에 데이터를 반영할 뷰 가져오기
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+
+//        뿌려줄 근거 데이터 가져오기
+        val data = mList[position]
+
+//      실 데이터 반영
+        topicTitleTxt.text = data.title
+
+        Glide.with(mContext).load(data.imageUrl).into(topicImg)
 
         return  row
     }
