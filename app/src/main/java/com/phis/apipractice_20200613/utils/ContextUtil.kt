ed01d102d13a17f0ed66da31705f58d75a14865a
parkcrystal <9jeworld@naver.com>
2020-06-14 11:06:24 +0900

    자동로그인 여부 저장 / 조회 기능 추가

--- app/src/main/java/com/phis/apipractice_20200613/utils/ContextUtil.kt
+++ app/src/main/java/com/phis/apipractice_20200613/utils/ContextUtil.kt
@@ -32,33 +32,29 @@ class ContextUtil {
 
         }
 
-    }
-
-
-    //      토큰 저장 기능
-    fun setUserToken(context: Context, token: String) {
+        fun setUserToken(context: Context, token: String) {
 
 //            저장할때 사용할 메모장 파일을 열자.
-        val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 //            열어준 메모장의 USER_TOKEN 항목에 받아온 token에 든 값을 저장.
-        pref.edit().putString(USER_TOKEN, token).apply()
+            pref.edit().putString(USER_TOKEN, token).apply()
 
-    }
+        }
 
-    //         저장된 토큰 불러오기
-    fun getUserToken(context: Context): String {
+        //         저장된 토큰 불러오기
+        fun getUserToken(context: Context): String {
 
 //            저장할때 사용한 메모장 파일을 열자.
-        val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 //          열어준 메모장의  USER_TOKEN 항목에 저장된 token값을 꺼내서 리턴
-        return pref.getString(
-            USER_TOKEN,
-            ""
-        )!!    // USER_TOKEN에 아무것도 없으면 ""으로 처리. 따라서, !! (널이 아니다) 처리해줌.
+            return pref.getString(
+                USER_TOKEN,
+                ""
+            )!!    // USER_TOKEN에 아무것도 없으면 ""으로 처리. 따라서, !! (널이 아니다) 처리해줌.
+
+        }
+
 
     }
-
-
-}
 
 }
