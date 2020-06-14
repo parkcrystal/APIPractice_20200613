2a6d19da1166b5e90b0fb71b0f63d6f5a8aba543
parkcrystal <9jeworld@naver.com>
2020-06-14 10:47:18 +0900

    내 닉네임 실제 파싱 출력

--- app/src/main/java/com/phis/apipractice_20200613/MainActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200613/MainActivity.kt
@@ -3,6 +3,7 @@ package com.phis.apipractice_20200613
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.phis.apipractice_20200613.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
@@ -26,6 +27,17 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
 
             override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val user = data.getJSONObject("user")
+                val nickName = user.getString("nick_name")
+
+                runOnUiThread {
+
+                    loginUserNickNameEdt.text = "${nickName}님 환영합니다."
+
+                }
+
 
             }
         })
