12ea01c83a918f28210ec14ef660712216ece1bb
parkcrystal <9jeworld@naver.com>
2020-06-20 10:16:22 +0900

    각 주제별 의견 내용만 우선 뿌려주기

--- app/src/main/java/com/phis/apipractice_20200613/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/phis/apipractice_20200613/adapters/ReplyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.phis.apipractice_20200613.R
 import com.phis.apipractice_20200613.datas.TopicReply
 
@@ -24,7 +25,18 @@ class ReplyAdapter(val mContext: Context, val resId: Int, val mList: List<TopicR
 
             tempRow = inf.inflate(R.layout.topic_reply_list_item, null)
         }
+
         var row = tempRow!!
+
+//        XML 에서 사용할 뷰 가져오기
+        val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+
+//        목록에서 뿌려줄 데이터 꺼내오기
+        val data = mList[position]
+
+//        데이터 / 뷰 연결 => 알고리즘
+        contentTxt.text = data.content
 
         return row
     }
